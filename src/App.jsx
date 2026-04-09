7e4c3bc761427dcf323926e496bdf5274fe222ac
Johan Calderon <yojancalderon3116@gmail.com>
2026-04-09 08:35:04 -0500

    Global Styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from "./index";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles></GlobalStyles>
+      <span>Hola</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as App } from './App';
+export * from './index';
+export * from './main';
+export * from './styles/GlobalStyles';
